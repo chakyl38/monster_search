201feaebd04a5e6ebe9a731613b2e5ca9c1555dd
Kyle Chapman <kylechapman09@gmail.com>
2020-05-13 16:04:31 -0500

    Updates

---
+++ precache-manifest.621471e9e38f3bf6a8e51b23e091f56c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6540ca789fdcfe7a70bf1539645fba49",
+    "url": "/monster_search/index.html"
+  },
+  {
+    "revision": "00b575d49d80ca88143c",
+    "url": "/monster_search/static/css/main.d052e539.chunk.css"
+  },
+  {
+    "revision": "5aee1ac6ecf24a38689d",
+    "url": "/monster_search/static/js/2.340cdc49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster_search/static/js/2.340cdc49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b575d49d80ca88143c",
+    "url": "/monster_search/static/js/main.64f19f00.chunk.js"
+  },
+  {
+    "revision": "3d00f85725bd59084d4b",
+    "url": "/monster_search/static/js/runtime-main.84b74cfe.js"
+  }
+]);
